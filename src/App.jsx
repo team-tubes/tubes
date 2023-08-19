b6930fb29cc892518ed3a4761500e5b14def301c
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2023-08-19 13:42:39 +1200

    📝 Add basic suburb flying

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {Map} from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 import DeckGL from '@deck.gl/react';
@@ -6,30 +6,31 @@ import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
 import {LightingEffect, AmbientLight, _SunLight as SunLight} from '@deck.gl/core';
 import {scaleThreshold} from 'd3-scale';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
+import { FlyToInterpolator } from 'deck.gl';
+import { bbox } from 'turf'
 
 
 // Source data GeoJSON
 const DATA_URL =
-  './Water_Hydrant.geojson'; // eslint-disable-line
+  './suburbs.geojson'; // eslint-disable-line
 
 const COLOR_SCALE = scaleThreshold()
   .domain([-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2])
   .range([
-    [65, 182, 196],
-    [127, 205, 187],
-    [199, 233, 180],
-    [237, 248, 177],
+    [65, 182, 196, 1000],
+    [127, 205, 187, 1000],
+    [199, 233, 180, 1000],
+    [237, 248, 177, 1000],
     // zero
-    [255, 255, 204],
-    [255, 237, 160],
-    [254, 217, 118],
-    [254, 178, 76],
-    [253, 141, 60],
-    [252, 78, 42],
-    [227, 26, 28],
-    [189, 0, 38],
-    [128, 0, 38]
+    [255, 255, 204, 1000],
+    [255, 237, 160, 1000],
+    [254, 217, 118, 1000],
+    [254, 178, 76, 1000],
+    [253, 141, 60, 1000],
+    [252, 78, 42, 1000],
+    [227, 26, 28, 1000],
+    [189, 0, 38, 1000],
+    [128, 0, 38, 1000]
   ]);
 
 const INITIAL_VIEW_STATE = {
@@ -40,6 +41,8 @@ const INITIAL_VIEW_STATE = {
   pitch: 45,
   bearing: 0
 };
+
+
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
 
@@ -83,6 +86,17 @@ function App({data = DATA_URL, mapStyle = MAP_STYLE}) {
     return [lightingEffect];
   });
 
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE)
+
+  const goToSuburb = useCallback((lon, lat) => {
+    setViewState({...viewState,
+      longitude: lon,
+      latitude: lat,
+      zoom: 15.5,
+      transitionInterpolator: new FlyToInterpolator({speed:0.1})
+    })
+  })
+
   const layers = [
     // only needed when using shadows - a plane for shadows to drop on
     new PolygonLayer({
@@ -95,13 +109,23 @@ function App({data = DATA_URL, mapStyle = MAP_STYLE}) {
     new GeoJsonLayer({
       id: 'geojson',
       data,
-      opacity: 0.8,
+      opacity: 0.01,
+      blendMode: 'overlay',
       stroked: false,
       filled: true,
-      extruded: true,
-      wireframe: true,
+      extruded: false,
+      wireframe: false,
+      getLineWidth: 3,
+      onClick: f => {
+        console.log(f.object.properties.name)
+        let suburbBbox = bbox(f.object.geometry)
+        // Calculate the center of the bounding box
+        const centerLatitude = (suburbBbox[1] + suburbBbox[3]) / 2; // Average latitude
+        const centerLongitude = (suburbBbox[0] + suburbBbox[2]) / 2; // Average longitude
+        goToSuburb(centerLongitude, centerLatitude)
+      },
       getElevation: f => Math.sqrt(f.properties.valuePerSqm) * 10,
-      getFillColor: f => COLOR_SCALE(f.properties.growth),
+      getFillColor: f => COLOR_SCALE((Math.random() * 1.8) - 0.6),
       getLineColor: [255, 255, 255],
       pickable: true
     })
@@ -124,7 +148,7 @@ function App({data = DATA_URL, mapStyle = MAP_STYLE}) {
     <DeckGL
       layers={layers}
       effects={effects}
-      initialViewState={INITIAL_VIEW_STATE}
+      initialViewState={viewState}
       controller={true}
     >
       <Map 

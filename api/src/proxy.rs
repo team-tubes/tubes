aaded7191e864aade0b02a633571ad6e7f8e8e81
Nathan  Hare <me@laspruca.nz>
2023-08-19 20:26:14 +1200

    Watercare fully proxied

--- api/src/proxy.rs
+++ api/src/proxy.rs
@@ -1,19 +1,17 @@
 use crate::ApiTags;
 use chrono::{DateTime, Utc};
 use poem::{http::StatusCode, Error, Result};
-use poem_openapi::{payload::Json, Object, OpenApi};
+use poem_openapi::{param::Path, payload::Json, Object, OpenApi};
 use reqwest::{Client, ClientBuilder};
 use serde::{Deserialize, Serialize};
 use tracing::error;
 
 #[derive(Object, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct OutageSummary {
+pub struct OutageCommon {
     #[oai(rename = "outageId")]
     outage_id: u32,
     location: String,
-    latitude: f64,
-    longitude: f64,
     #[oai(rename = "startDate")]
     start_date: Option<DateTime<Utc>>,
     #[oai(rename = "endDate")]
@@ -24,6 +22,27 @@ pub struct OutageSummary {
     tbc_start: bool,
     #[oai(rename = "tbcEnd")]
     tbc_end: bool,
+}
+
+#[derive(Object, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OutageSummary {
+    latitude: f64,
+    longitude: f64,
+    #[serde(flatten)]
+    #[oai(flatten)]
+    common: OutageCommon,
+}
+
+#[derive(Object, Serialize, Deserialize)]
+pub struct OutageDetailed {
+    duration: u32,
+    #[oai(rename = "propertiesAffected")]
+    properties_affected: u32,
+    description: String,
+    #[serde(flatten)]
+    #[oai(flatten)]
+    common: OutageCommon,
 }
 
 pub struct Proxy {
@@ -43,6 +62,7 @@ impl Proxy {
 
 async fn reqwest_to_poem(request: reqwest::Response) -> poem::Response {
     let mut response = poem::Response::default();
+    response.set_status(request.status());
     let headers = response.headers_mut();
 
     for (header, value) in request.headers() {
@@ -64,6 +84,32 @@ impl Proxy {
         let result = self
             .client
             .get("https://api.watercare.co.nz/outages/all")
+            .send()
+            .await
+            .map_err(|ex| {
+                error!("Could not get all watercare outages {ex}");
+                Error::from_status(StatusCode::INTERNAL_SERVER_ERROR)
+            })?;
+
+        if result.status() != StatusCode::OK {
+            return Err(Error::from_response(reqwest_to_poem(result).await));
+        }
+
+        Ok(Json(result.json().await.map_err(|ex| {
+            error!("Could not get all watercare outages {ex}");
+            Error::from_status(StatusCode::INTERNAL_SERVER_ERROR)
+        })?))
+    }
+
+    #[oai(path = "/watercare/:id", method = "get")]
+    /// Proxies [https://api.watercare.co.nz/outages/all/:id/details](https://api.watercare.co.nz/outages/:id/details) to avoid cors issues to
+    async fn get_watercare_detailed(&self, id: Path<u32>) -> Result<Json<Vec<OutageDetailed>>> {
+        let result = self
+            .client
+            .get(format!(
+                "https://api.watercare.co.nz/outages/{}/outage",
+                id.0
+            ))
             .send()
             .await
             .map_err(|ex| {

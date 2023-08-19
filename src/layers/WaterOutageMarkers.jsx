66f1ba1582b377780105f725780bccc55de8674b
FilUnderscore <fil@filunderscore.com>
2023-08-20 11:06:20 +1200

    refactoring - consolidate water outage marker data into the layer component file

--- src/layers/WaterOutageMarkers.jsx
+++ src/layers/WaterOutageMarkers.jsx
@@ -2,8 +2,6 @@ import { Marker, Popup } from "react-map-gl";
 import { useState, useEffect } from "react";
 import { format, parseISO, intervalToDuration } from "date-fns";
 
-import { get_auckland_council_water_outages } from "../layers/WaterLayer";
-
 export const WaterOutageMarkers = () => {
   const [waterOutageData, setWaterOutageData] = useState([]);
 
@@ -90,3 +88,27 @@ const WaterOutageMarker = (outage) => {
     </>
   );
 };
+
+async function get_auckland_council_water_outages() {
+  //Get all outages
+  const response = await fetch("https://api.infra.nz/api/watercare/all");
+  const all_outages = await response.json();
+
+  //We then want to get the specific water outages data
+  const all_outages_with_data = await Promise.all(
+    all_outages.map(async (item) => ({
+      ...item,
+      outage_data: await get_water_outage_data_specific(item.outageId),
+    }))
+  );
+  return all_outages_with_data;
+}
+
+function get_water_outage_data_specific(outage_id) {
+  //Get all outages
+  return fetch("https://api.infra.nz/api/watercare/" + outage_id)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}

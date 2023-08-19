d3e19ffbef2b87aee0180ca415fd1c94fa8be97b
Tony Cui <ycui982@aucklanduni.ac.nz>
2023-08-19 14:11:35 +1200

    feat: added search

--- src/components/IssueCard.jsx
+++ src/components/IssueCard.jsx
@@ -1,18 +1,19 @@
+import { titleCase } from "../utils/utils";
 
-export default function IssueCard({ issue }) {
+export default function IssueCard({ suburb, issues }) {
 	return (
 		<div className="md:mb-2">
 			<div className="flex flex-col rounded-xl bg-neutral-800 py-2 px-5 relative shadow-md hover:cursor-pointer">
 				<h6 className="flex-grow font-extrabold md:text-xl md:py-3 text-purple-200">
-					Avondale
+					{titleCase(suburb)}
 				</h6>
 				<hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
 				<div className="my-2">
 					<p className="font-medium my-2 text-sm md:text-md text-gray-200">
-						3 Active Issues
+						{issues.length} Active Issues
 					</p>
 					<p className="font-medium my-2 text-sm md:text-md text-gray-200">
-						123 Assets Available
+						0 Assets Available
 					</p>
 				</div>
 			</div>
--- src/pages/IssuePage.jsx
+++ src/pages/IssuePage.jsx
@@ -1,35 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import IssueList from '../components/IssueList';
 import Search from '../components/Search';
 import { AppContext } from '../context/AppContextProvider';
+import suburbs from '../assets/suburbs.json';
 
 export default function IssuePage() {
 	const { outages, outagesLoading, refresh } = useContext(AppContext);
+	const [searchParam] = useState(['suburb']);
+	const [query, setQuery] = useState('');
 
 	if (outagesLoading) return <div>loading...</div>;
 
+	const suburbsData = suburbs.data;
+
 	function buildIssueMap(outages) {
 		const issueMap = new Map();
+		for (const currSuburb of suburbsData) {
+			issueMap.set(currSuburb, []);
+		}
 		for (const outage of outages) {
-			const suburb = outage.location.split(',').slice(1)[0].trim();
+			const suburb = outage.location
+				.split(',')
+				.slice(1)[0]
+				.trim()
+				.toLowerCase();
 			const issue = {
 				...outage,
 				type: 'outage',
 			};
-			if (!issueMap.has(suburb)) {
-				issueMap.set(suburb, []);
+			if (issueMap.has(suburb)) {
+				issueMap.get(suburb).push(issue);
+				// console.log("has suburb: " + suburb)
+			} else {
+				// console.log("no suburb: " + suburb)
 			}
-			issueMap.get(suburb).push(issue);
 		}
 		return issueMap;
 	}
+
+	const search = (issues) => {
+		const issueMap = buildIssueMap(issues);
+		const matched = new Map(
+			[...issueMap].filter(
+				([k, v]) => k.toLowerCase().indexOf(query.toLowerCase()) > -1
+			)
+		);
+
+		return matched;
+	};
 
 	return (
 		<div className="w-full flex justify-center">
 			<div className="container mx-auto px-2 md:px-36 flex justify-center flex-col items-center">
 				<div className="h-12" />
-				<Search />
-				<IssueList issues={buildIssueMap(outages)} />
+				<Search value={query} setValue={setQuery}/>
+				<IssueList issuesMap={search(outages)} />
 			</div>
 		</div>
 	);

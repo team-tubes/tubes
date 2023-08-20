66ecbf81cb218be201e0d2d07b9264e355fe6680
Nathan  Hare <me@laspruca.nz>
2023-08-20 12:26:28 +1200

    Fuk no ahhhhhhh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { NavItem } from './NavItem';
+import React, { useState } from "react";
+import { NavItem } from "./NavItem";
 
 export default function Navbar() {
-	return (
-		<nav className="sticky items-center top-0 z-20 flex flex-wrap justify-between bg-neutral-800 x-2 drop-shadow-lg px-4 py-3">
-			<a href="#" className="text-lg font-bold">
-				infra.nz
-			</a>
-			<ul className="flex flex-wrap">
-				<NavItem path={'/'} text="Map" />
-				<NavItem path={'/issues'} text="Suburbs" />
-				<NavItem path={'/report'} text="Report" />
-				<NavItem path={'/upload'} text="Upload" />
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="sticky items-center top-0 z-20 flex flex-wrap justify-between bg-neutral-800 x-2 drop-shadow-lg px-4 py-3">
+      <a href="#" className="text-lg font-bold">
+        infra.nz
+      </a>
+      <ul className="flex flex-wrap">
+        <NavItem path={"/"} text="Map" />
+        <NavItem path={"/issues"} text="Suburbs" />
+        <NavItem path={"/report"} text="Report" />
+        <NavItem path={"/upload"} text="Upload" />
+        <NavItem path={"/reports"} text="All Reports" />
+      </ul>
+    </nav>
+  );
 }
---
+++ src/pages/AllReportsPage.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from "react";
+import IssueList from "../components/IssueList";
+import Search from "../components/Search";
+import ReportCard from "../components/ReportCard";
+
+async function getReports(offset, limit) {
+  console.log("Reports win");
+
+  const reports = await fetch(
+    `https://api.infra.nz/api/complaints?${
+      offset ? "offset=" + offset + "&" : ""
+    }${limit ? "limit=" + limit : ""}`,
+    {
+      method: "GET",
+    }
+  );
+
+  if (reports.status != 200) {
+    return [];
+  }
+
+  return await reports.json();
+}
+
+export default function AllReportsPage() {
+  const [loading, setLoading] = useState(true);
+  const [reports, setReports] = useState();
+
+  useEffect(() => {
+    getReports().then((reports) => {
+      console.log(reports);
+      setReports(reports);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <div>loading...</div>;
+  console.log({ reports });
+  const elements = reports.map(({ id, ...rest }) => (
+    <ReportCard key={id} {...rest} />
+  ));
+
+  return (
+    <div className="w-full max-w-[1600px] mx-auto">
+      <div className="px-6  grid grid-cols-3 grid-rows-auto gap-4 p-4">
+        {elements}
+      </div>
+    </div>
+  );
+}
